56f5322dfdd438cc36110e813dc1c39206bdb2f5
carlos <carlosfdeztessier@gmail.com>
2015-07-02 11:18:28 +0200

    actualizacion

--- HolaMundo/src/com/example/paqueteServlets/HelloWorld.java
+++ HolaMundo/src/com/example/paqueteServlets/HelloWorld.java
@@ -1,7 +1,6 @@
 package com.example.paqueteServlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,8 +16,17 @@ public class HelloWorld extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
-		PrintWriter out =response.getWriter();
-		out.print("Hola mundo");
+		java.io.PrintWriter out =response.getWriter();
+		out.print("<html>");
+		out.print("<head>");
+		out.print("<title>HelloWorld</title>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<div style='text-align:center'>");
+		out.print("<h1 style='color:red'>Hola Mundo</h1>");
+		out.print("<img width='400px' src='https://pbs.twimg.com/media/BCh-v51CAAA9HuT.jpg'/>");
+		out.print("</body>");
+		out.print("</html>");
 		out.close();
 	}
 
