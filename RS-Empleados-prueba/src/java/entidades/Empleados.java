1a4bd63cf58a71d0f130eab5f30222d6b70d7e2a
carlos <carlosfdeztessier@gmail.com>
2015-07-10 12:46:25 +0200

    commit inicial

---
+++ RS-Empleados-prueba/src/java/entidades/Empleados.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author alumno
+ */
+@Entity
+@Table(name = "empleados")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Empleados.findAll", query = "SELECT e FROM Empleados e"),
+    @NamedQuery(name = "Empleados.findByIdempleado", query = "SELECT e FROM Empleados e WHERE e.idempleado = :idempleado"),
+    @NamedQuery(name = "Empleados.findByNombre", query = "SELECT e FROM Empleados e WHERE e.nombre = :nombre"),
+    @NamedQuery(name = "Empleados.findByTelefono", query = "SELECT e FROM Empleados e WHERE e.telefono = :telefono")})
+public class Empleados implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idempleado")
+    private Integer idempleado;
+    @Size(max = 64)
+    @Column(name = "nombre")
+    private String nombre;
+    @Size(max = 9)
+    @Column(name = "telefono")
+    private String telefono;
+
+    public Empleados() {
+    }
+
+    public Empleados(Integer idempleado) {
+        this.idempleado = idempleado;
+    }
+
+    public Integer getIdempleado() {
+        return idempleado;
+    }
+
+    public void setIdempleado(Integer idempleado) {
+        this.idempleado = idempleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idempleado != null ? idempleado.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Empleados)) {
+            return false;
+        }
+        Empleados other = (Empleados) object;
+        if ((this.idempleado == null && other.idempleado != null) || (this.idempleado != null && !this.idempleado.equals(other.idempleado))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entidades.Empleados[ idempleado=" + idempleado + " ]";
+    }
+    
+}
